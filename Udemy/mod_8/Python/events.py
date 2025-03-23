ed26ee453ef0f8a3ac0c5e0f6a4b545be57b1f0e
LucasMorgani <morganis1411@gmail.com>
2025-03-23 13:24:40 -0300

    Adicionando webhook do discord em python

--- Udemy/mod_8/Python/events.py
+++ Udemy/mod_8/Python/events.py
@@ -1,4 +1,23 @@
 import docker
+import datetime
+import requests
+from dotenv import load_dotenv
 
+
+# Unix sock version
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+# TCP sock version
+# client = docker.DockerClient(base_url='TPC://127.0.0.1:2375')
+webhook_url = "https://discord.com/api/webhooks/1353397518549123223/5BfnlBpGNZ8xUxCk01Xd_Hvo39hr_w6jeG7PgB9CZdC9rjdOXBv8BYvJZ5o_b7CGBBQI"
 
+
+for event in client.events(decode=True, filters={"event": "die"}):
+    container_id = event["id"]
+    container_name = event["Actor"]["Attributes"]["name"]
+    epoch_time = event["time"]
+    date_time = datetime.datetime.fromtimestamp(epoch_time)
+
+    payload =  {"content": "O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time)}
+
+    print (payload)
+    requests.post(webhook_url, data=payload)
